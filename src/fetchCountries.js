bd6c2eb058ae4c050ff9e2c9b5d94fe66aa53709
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2023-01-08 17:22:48 +0200

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,8 @@
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+
+
 export function fetchCountries(name) {
     
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
